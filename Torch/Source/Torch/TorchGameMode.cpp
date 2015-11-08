d7a1ff85d8079d9adfa4eac65597add79984874f
RomekRJM <rjm_20@o2.pl>
2015-11-08 22:19:22 +0000

    Boilerplate for Torch project.

---
+++ Torch/Source/Torch/TorchGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Torch.h"
+#include "TorchGameMode.h"
+
+
+
+
---
+++ Torch/Source/Torch/TorchGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TorchGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TORCH_API ATorchGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
