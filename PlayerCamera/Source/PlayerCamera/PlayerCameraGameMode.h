f5cf1db2bff97986fb88f109912a74ae5da2422f
RomekRJM <rjm_20@o2.pl>
2015-11-21 21:22:49 +0000

    New project: pawn with attached camera.

---
+++ PlayerCamera/Source/PlayerCamera/PlayerCameraGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PlayerCameraGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYERCAMERA_API APlayerCameraGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
