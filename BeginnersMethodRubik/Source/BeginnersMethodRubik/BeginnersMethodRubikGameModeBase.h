746527a102b5f2de5226c3e9e0ea2c7b19e0ca75
Roman Subik <romek.rjm@gmail.com>
2018-07-28 20:19:38 +0200

    BeginnersMethodRubik: cube visualisation.

---
+++ BeginnersMethodRubik/Source/BeginnersMethodRubik/BeginnersMethodRubikGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BeginnersMethodRubikGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEGINNERSMETHODRUBIK_API ABeginnersMethodRubikGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
